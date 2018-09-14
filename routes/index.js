c9f592401313e41ad9c572fe26e8a6ebdeb4240f
AvichalChadha <avichalchadha1999@gmailcom>
2018-09-14 17:36:12 +0530

    all the issues solved. working on scraping the keywords like qid, rf and fromHash

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,101 @@ const JSON = require('circular-json');
 var request = require('request');
  
 
-router.get('/lol', function (req, res) {
+
+
+
+
+
+
+router.get('/lol', function(req, res,next){
+    url =  "https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=xiaomi"
+    //view-source:https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=xiaomi&rh=i%3Aaps%2Ck%3Axiaomi
+    headers =   {
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+        "X-user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 FKUA/website/41/website/Desktop",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Connection": "keep-alive"
+        }
+
+    var response = axios({
+        method: 'post',
+        url: url,
+        headers:headers
+      })
+    .then(function (response){
+        //console.log(response.data)
+        amazon_webpage = response.data
+        //&rh=i%3Aaps%2Ck%3Axbox+games+
+       // i%3Aapparel%2Cn%3A1571271031%2Cp_4%3AJust%20F%20by%20Jacqueline%20Fernandez%2Cp_98%3A10440597031
+        //&fromHash=%2Fref%3Dsr_il_ti_videogames%3Ffst%3Das%253Aon%26rh%3Dk%253Aps4%2Bgames%252Cn%253A976460031%252Cn%253A2591138031%252Cn%253A2591141031%26keywords%3Dps4%2Bgames%26ie%3DUTF8%26qid%3D1536874220%26lo%3Dvideogames
+        //&oqid=1536874409
+        regex_rh= /rh=(.*?)&.*?xiaomi/g
+        let x;
+        if ((x = regex_rh.exec(amazon_webpage)) !== null) {
+                if (x.index === regex_rh.lastIndex) {
+                    regex.lastIndex++;
+                }
+            console.log(x[1])
+        }
+        let y 
+        regex_qid = /qid=(.*?)\">/
+        if ((y = regex_rh.exec(amazon_webpage)) !== null) {
+            if (y.index === regex_rh.lastIndex) {
+                regex.lastIndex++;
+            }
+        console.log(y[1])
+        }
+        //https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=xiaomi&rh=i%3Aaps%2Ck%3Axiaomi&fromHash=%2Fref%3Dnb_sb_noss_2%3Furl%3Dsearch-alias%253Daps%26field-keywords%3Dpixel%26rh%3Di%253Aaps%252Ck%253Apixel&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536922216&atfLayout=list
+        res.send('lol')
+    })
+    
+
+        
+
+    
+    .catch(function(err){
+          console.log('this is error',err)
+      })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/lol2', function (req, res) {
     //url= "https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=iphone&rh=i%3Aaps%2Ck%3Aiphone&fromHash=%2Fref%3Dnb_sb_noss_2%3Furl%3Dsearch-alias%253Daps%26field-keywords%3Dsamsing%2B%26sprefix%3Dsamsing%252Caps%252C380%26crid%3DXQW3OYZSXV6C&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536522016&atfLayout=list"
     //samsung phones 
     //url = "https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss?url=node%3D1805560031&field-keywords=samsung&lo=electronics&rh=n%3A1805560031%2Ck%3Asamsung&fromHash=%2Fs%2Fref%3Dsr_nr_p_89_2%3Ffst%3Das%253Aoff%26rh%3Dn%253A976419031%252Cn%253A1389401031%252Cn%253A1389432031%252Cn%253A1805560031%252Ck%253Aiphone%252Cp_89%253AOnePlus%26keywords%3Diphone%26ie%3DUTF8%26qid%3D1536522399%26rnid%3D3837712031%26lo%3Delectronics&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536675080&atfLayout=image"
@@ -16,8 +110,12 @@ router.get('/lol', function (req, res) {
     //1st sept , below 2 are not working 
     //url = 'https://www.amazon.in/mn/search/ajax/&rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1389432031%2Ck%3Aphones%2Cp_n_operating_system_browse-bin%3A1485080031&page=2&keywords=phones&ie=UTF8&qid=1535808244&fromHash=%2Fgp%2Fsearch%2Fref%3Dsr_nr_p_n_operating_system_0%3Ffst%3Das%253Aoff%26rh%3Dn%253A976419031%252Cn%253A1389401031%252Cn%253A1389432031%252Ck%253Aphones%252Cp_n_operating_system_browse-bin%253A1485080031%26keywords%3Dphones%26ie%3DUTF8%26qid%3D1535808224&section=BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1535808434&atfLayout=list'
     //url = "https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss?url=node%3D1805560031&field-keywords=jbl+speakers&rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Ck%3Ajbl+speakers&fromHash=%2Fs%2Fref%3Dsr_nr_p_n_feature_six_brow_2%3Ffst%3Das%253Aoff%26rh%3Dn%253A976419031%252Cn%253A1389401031%252Cn%253A1389432031%252Cn%253A1805560031%252Ck%253Avivo%252Cp_n_feature_six_browse-bin%253A6631754031%26keywords%3Dvivo%26ie%3DUTF8%26qid%3D1536341840%26rnid%3D6631751031&section=BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536357100&atfLayout=list"
-    url = 'https://www.amazon.in/mn/search/ajax/s/ref=sr_nr_p_89_0?fst=as%3Aoff&rh=n%3A1350384031%2Cn%3A1374515031%2Ck%3Awashing+machine+cleaning+powder%2Cp_89%3AWhirlpool&keywords=washing+machine+cleaning+powder&ie=UTF8&qid=1536825684&rnid=3837712031&fromHash=%2Fref%3Dnb_sb_ss_i_2_16%3Furl%3Dsearch-alias%253Daps%26field-keywords%3Dwashing%2Bmachine%2Bcleaning%2Bpowder%26sprefix%3Dwashing%2Bmachine%2B%252Caps%252C322%26crid%3D2Z7HAV5X3M10V&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536825697&atfLayout=grid'
-    
+    //url = 'https://www.amazon.in/mn/search/ajax/s/ref=sr_nr_p_89_0?fst=as%3Aoff&rh=n%3A1350384031%2Cn%3A1374515031%2Ck%3Awashing+machine+cleaning+powder%2Cp_89%3AWhirlpool&keywords=washing+machine+cleaning+powder&ie=UTF8&qid=1536825684&rnid=3837712031&fromHash=%2Fref%3Dnb_sb_ss_i_2_16%3Furl%3Dsearch-alias%253Daps%26field-keywords%3Dwashing%2Bmachine%2Bcleaning%2Bpowder%26sprefix%3Dwashing%2Bmachine%2B%252Caps%252C322%26crid%3D2Z7HAV5X3M10V&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536825697&atfLayout=grid'
+    //urls for testing 
+    //url = "https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss?url=node%3D1389432031&field-keywords=ps4+games&rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1389432031%2Ck%3Aps4+games&fromHash=%2Fgp%2Fsearch%2Fref%3Dsr_nr_p_89_0%3Ffst%3Das%253Aoff%26rh%3Dn%253A976419031%252Cn%253A1389401031%252Cn%253A1389432031%252Ck%253Aiphones%252Cp_89%253AApple%26keywords%3Diphones%26ie%3DUTF8%26qid%3D1536865785%26rnid%3D3837712031&section=BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536866186&atfLayout=grid"
+    //url = 'https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss?url=node%3D1389432031&field-keywords=xbox+games&lo=videogames&section=BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&atfLayout=list'
+    //url = 'https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=xbox+games+&lo=videogames&rh=i%3Aaps%2Ck%3Axbox+games+&fromHash=%2Fref%3Dsr_il_ti_videogames%3Ffst%3Das%253Aon%26rh%3Dk%253Aps4%2Bgames%252Cn%253A976460031%252Cn%253A2591138031%252Cn%253A2591141031%26keywords%3Dps4%2Bgames%26ie%3DUTF8%26qid%3D1536874220%26lo%3Dvideogames&section=BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536357100&atfLayout=grid'
+    url = "https://www.amazon.in/mn/search/ajax/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=pixel&rh=i%3Aaps%2Ck%3Apixel&fromHash=%2Fref%3Dnb_sb_noss_2%3Furl%3Dsearch-alias%253Daps%26field-keywords%3Dmoto%2B%26rh%3Di%253Aaps%252Ck%253Amoto%2B&section=ATF,BTF&fromApp=gp%2Fsearch&fromPage=results&fromPageConstruction=auisearch&version=2&oqid=1536920521&atfLayout=list"
     headers =   {
         "Accept-Language": "en-US,en;q=0.5",
         "Content-Type": "application/json",
@@ -51,120 +149,102 @@ router.get('/lol', function (req, res) {
         //scrapping the key having most of the products. 
         
         
-        max_products = json_response_data.centerBelowPlus.data.value
-        min_products = json_response_data.centerMinus.data.value
+        
+        
         //console.log(max_products)
-        if (max_products == null){
+        console.log("HELP")
+        if (!("centerBelowPlus" in json_response_data)){
+            min_products = json_response_data.centerMinus.data.value
             data_array = [min_products]
-            console.log('no data in  max_products')
+            console.log('No data in  max_products')
         }
-        else if (min_products == null){
+        else if (!("centerMinus" in json_response_data )){
+            max_products = json_response_data.centerBelowPlus.data.value
             data_array = [max_products]
+
             console.log("no data in min_products")
         }
         else{
             console.log("all data ")
-                    //console.log(data_array[i])
+            //console.log(data_array[i])
+            min_products = json_response_data.centerMinus.data.value
+            max_products = json_response_data.centerBelowPlus.data.value
             data_array = [max_products, min_products ]
-            console.log("lol")
-            for (i in data_array){
-                //console.log(data_array[i])
-                const $ = cheerio.load(data_array[i])
-                const product_name = $('.s-access-title')
-            
-
-            
-                product_name.each(function(index,product){
-
-                    const product_title = $(product).text()
-                    console.log(product_title)
-                    console.log("---------------------------")
-
-
-                    //remove the sponsered result. 
-                    let m
-                    const sponsored_regex = /\[Sponsored]/g;
-                    if ((m = sponsored_regex.exec(product_title)) !== null) {
-                    // This is necessary to avoid infinite loops with zero-width matches
-                        if (m.index === sponsored_regex.lastIndex) {
+        }
+        console.log("lol")
+        for (i in data_array){
+            //console.log(data_array[i])
+            const $ = cheerio.load(data_array[i])
+            const product_name = $('.s-access-title')
+        
+        
+            product_name.each(function(index,product){
+                const product_title = $(product).text()
+                console.log(product_title)
+                console.log("---------------------------")
+                //remove the sponsered result. 
+                let m
+                const sponsored_regex = /\[Sponsored]/g;
+                if ((m = sponsored_regex.exec(product_title)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                    if (m.index === sponsored_regex.lastIndex) {
+                        regex.lastIndex++;
+                    }
+                    console.log("alert, sponsored product found")
+                }   
+                else{
+                    var newData1 = product_title.replace(/\(/gm,'\\(')
+                    var newData2 = newData1.replace(/\)/gm,'\\)')
+                    var newData3 = newData2.replace(/\+/gm,'\\+')
+                    //console.log(newData3)
+                    const regex_url = new RegExp('<a .*? title=\"' + newData3 +  '\\".*?href=\\"(.*?)\\">' , 'g')
+                    console.log(regex_url)
+                    console.log("outside while loop ")
+                    if ((m = regex_url.exec(data_array[i])) !== null) {
+                        // This is necessary to avoid infinite loops with zero-width matches
+                        if (m.index === regex_url.lastIndex) {
                             regex.lastIndex++;
                         }
-                        console.log("alert, sponsored product found")
-
-                    }   
-
-                    else{
-
-
-
-                        var newData1 = product_title.replace(/\(/gm,'\\(')
-                        var newData2 = newData1.replace(/\)/gm,'\\)')
-                        var newData3 = newData2.replace(/\+/gm,'\\+')
-
-
-                        //console.log(newData3)
-
-                        const regex_url = new RegExp('<a .*? title=\"' + newData3 +  '\\".*?href=\\"(.*?)\\">' , 'g')
-
-                        console.log(regex_url)
-
-                        console.log("outside while loop ")
-                        if ((m = regex_url.exec(data_array[i])) !== null) {
-
+                        console.log("????????????")
+                        
+                    
+                    
+                        const regex_image = new RegExp('<img src=\\"(.*?.jpg)\\" .*? alt=\\"' +  newData3 + '\\" class=\\"s-access-image', 'g' )
+                        console.log(regex_image)
+                        console.log("outside2 while loop ")
+                        let data
+                        if ((data = regex_image.exec(data_array[i]))!== null) {
+                            console.log("----------------------------------------------")
                             // This is necessary to avoid infinite loops with zero-width matches
-                            if (m.index === regex_url.lastIndex) {
-                                regex.lastIndex++;
+                            if (data.index === regex_image.lastIndex) {
+                                regex_image.lastIndex++;
                             }
-
-                            console.log("????????????")
-                            //console.log(m[1])
-                        
-                        
-                            const regex_image = new RegExp('<img src=\\"(.*?.jpg)\\" .*? alt=\\"' +  newData3 + '\\" class=\\"s-access-image', 'g' )
-                            console.log(regex_image)
-
-                            console.log("outside2 while loop ")
-
-                            let data
-                            if ((data = regex_image.exec(data_array[i]))!== null) {
-                                console.log("----------------------------------------------")
-                                // This is necessary to avoid infinite loops with zero-width matches
-                                if (data.index === regex_image.lastIndex) {
-                                    regex_image.lastIndex++;
-
-                                }
-                                console.log("lol")
-                                //console.log(data[1])
-
-                                console.log("```````````````````````````````````````````````")
-
-                                json_obj['amazon_product'].push({"name" : product_title, "product_url" :m[1], "product_image_url":data[1] });
-                                json_str = JSON.stringify(json_obj)
-
-                                //console.log(json_str)  
-                            }
-                            else{
-                                console.lof("no image url found")
-                            }
-
-                        
+                            console.log("lol")
+                            //console.log(data[1])
+                            console.log("```````````````````````````````````````````````")
+                            json_obj['amazon_product'].push({"name" : product_title, "product_url" :m[1], "product_image_url":data[1] });
+                            json_str = JSON.stringify(json_obj)
+                            //console.log(json_str)  
                         }
                         else{
-                            console.log("no url found")
+                            console.lof("no image url found")
                         }
-
+                    
                     }
-
-                })
-
-
-            }
-
+                    else{
+                        console.log("no url found")
+                    }
+                }
+            })
         }
-        
-    res.send(json_response_data)
-    //res.send(json_str)
 
+        
+    //this is all the data including html
+    //res.send(json_response_data)
+    //this is all the poducts, links, images 
+    //res.send(json_str)
+    //this is just the raw html 
+    res.send(max_products)
         
 
       })
@@ -175,11 +255,6 @@ router.get('/lol', function (req, res) {
     
   })
   
-
-
-
-
-
 
 
 
@@ -392,7 +467,7 @@ router.get('/d', function(req,res){
                 }
                 
             
-        res.send(max_products)
+        //res.send(max_products)
             //const product_link = $()
             //res.send(max_products)  
     
